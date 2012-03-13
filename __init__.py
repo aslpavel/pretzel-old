606432adbd34fc044b08dfdae3b80b6db6edc240
aslpavel <asl.pavel@gmail.com>
2012-03-13 20:42:31 +0400

    [tests] added: remoting tests

--- __init__.py
+++ __init__.py
@@ -7,11 +7,12 @@ def load_tests (loader, tests, pattern):
     from unittest import TestSuite
 
     from . import async
-    from . import log
     from . import udb
+    from . import log
+    from . import remoting
 
     suite = TestSuite ()
-    for test in (async, log, udb):
+    for test in (async, log, udb, remoting):
         suite.addTests (loader.loadTestsFromModule (test))
 
     return suite
