aac383a1a9fd3745e68944514b58ff98124de682
aslpavel <asl.pavel@gmail.com>
2012-05-17 14:18:28 +0400

    [glib.GEvent] added: optional connect arguments

--- glib/event.py
+++ glib/event.py
@@ -6,19 +6,20 @@ __all__ = ('GEvent',)
 # GObject Event                                                                #
 #------------------------------------------------------------------------------#
 class GEvent (event.BaseEvent):
-    __slots__ = ('source', 'name')
+    __slots__ = ('source', 'name', 'args',)
 
-    def __init__ (self, source, name):
+    def __init__ (self, source, name, *args):
         self.source = source
         self.name   = name
+        self.args   = args
 
     #--------------------------------------------------------------------------#
     # Add | Remove                                                             #
     #--------------------------------------------------------------------------#
     def Add (self, handler):
-        return self.source.connect (self.name, handler)
+        return self.source.connect (self.name, handler, *self.args)
 
     def Remove (self, handler_id):
-        return self.source.disconnect (handler_id)
+        return self.source.disconnect (handler_id, *self.args)
 
 # vim: nu ft=python columns=120 :
